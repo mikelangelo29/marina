7ac262bf14743e8381334035251a68e12bb863a8
mikelangelo29 / michelangelo + zanelli <michelangelozanelli@gmail.com>
2023-07-10 10:01:13 +0200

    time

--- marina/news/models.py
+++ marina/news/models.py
@@ -4,9 +4,12 @@ class News(models.Model):
     immagine=models.ImageField()
     titolo=models.CharField(max_length=20, default=None)
     descrizione=models.TextField()
+    data_creazione= models.DateTimeField(auto_now_add=True)
     firma= models.CharField(max_length=30, default=None)
     
-    
-    
+    class Meta:
+        ordering=['-data_creazione']
+
     def __str__(self):
         return self.titolo
+    
